020191d10e1764aab0469c959e2e5b837cc0de90
stephen <stephenfanale@hotmail.com>
2019-02-04 11:08:29 -0500

    -- visual changes to nav bar

--- big-house-website/src/app/app.component.ts
+++ big-house-website/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit{
 
   currentUser: User;
   currentUserSubscription: Subscription;
-  users: User[] = [];
+  users: User[] = null;
 
   constructor(
     private authenticationService: AuthenticationService,
